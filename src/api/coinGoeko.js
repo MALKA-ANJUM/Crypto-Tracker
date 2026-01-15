88f726407741fd1f3fdc0a09dd3c1aaa30262501
Malka Anjum <malkaanjum1999@gmail.com>
2026-01-15 22:03:59 +0530

    Refactor API calls and enhance CryptoCard with routing to detail page

--- src/api/coinGoeko.js
+++ src/api/coinGoeko.js
@@ -2,9 +2,21 @@
 
 const BASE_URL = "https://api.coingecko.com/api/v3";
 
-const fetchCrypto = async () => {
+export const fetchCrypto = async () => {
     const response = await fetch(
         `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`,
+    );
+
+    if(!response.ok){
+        throw new Error("Failed to fetch cryptos");
+    };
+
+    return response.json();
+}
+
+export const fetchCryptoData = async (id) => {
+    const response = await fetch(
+        `${BASE_URL}/coins/${id}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`,
     );
 
     if(!response.ok){
--- src/pages/CointDetail.jsx
+++ src/pages/CointDetail.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router'
 
 const CointDetail = () => {
+  const { id } = useParams();
+  console.log(id);
+  
+  
   return (
     <div>
-      
+      thisis coin detail page: {id}
     </div>
   )
 }
