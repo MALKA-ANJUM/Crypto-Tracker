b8109ecfd340849ab467f2153d692dfb2688add8
Malka Anjum <malkaanjum1999@gmail.com>
2026-01-18 22:40:48 +0530

    Implement coin detail fetching and loading state in CointDetail component

--- src/pages/CointDetail.jsx
+++ src/pages/CointDetail.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { fetchCryptoData } from '../api/coinGoeko';
 
 const CointDetail = () => {
-  const { id } = useParams();
-  console.log(id);
-  
-  
-  return (
-    <div>
-      thisis coin detail page: {id}
-    </div>
-  )
+	const { id } = useParams();
+	const [ coin, setCoin ] = useState(null);
+	const [ isLoading, setIsLoading ] = useState(true);
+
+	useEffect(() => {
+		loadCoinData();
+	}, [id]);
+
+	const loadCoinData = async () => {
+		try {
+			const data = await fetchCryptoData();
+			setCoin(data);
+		} catch (error) {
+			console.error("Error fetching crypto: ", error)
+		} finally {
+			setIsLoading(false);
+		}
+	}
+	
+	if(!coin){
+		return (
+			<div className="app">
+				<div className="no-results">
+					<p>Coin not found</p>
+					<button>Go Back</button>
+				</div>
+			</div>
+		)
+	}
+	
+	return (
+		<div>
+		this is coin detail page: {id}
+		</div>
+	)
 }
 
 export default CointDetail
